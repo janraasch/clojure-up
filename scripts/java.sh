83acdaa12e2fadfd4ff1d6900da8b99da5dde669
Jan Raasch <jan@janraasch.com>
2016-03-05 18:43:20 +0100

    feat: install java 8
    This might not work in non-interactive setup. Will try this next time.

--- scripts/java.sh
+++ scripts/java.sh
@@ -1,19 +1,20 @@
 ## Java
 echo 'start installing java'
+apt-add-repository ppa:webupd8team/java
 apt-get update
-apt-get install -y openjdk-7-jdk
+apt-get install -y oracle-java8-installer
 
 bash_profile='~/.bash_profile'
 v_bash_profile="${home_vagrant}/.bash_profile"
 fish_config='~/.config/fish/config.fish'
 v_fish_config="${home_vagrant}/.config/fish/config.fish"
 
-echo 'export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-i386' >> $bash_profile
-echo 'export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-i386' >> $v_bash_profile
+echo 'export JAVA_HOME=/usr/lib/jvm/java-8-oracle' >> $bash_profile
+echo 'export JAVA_HOME=/usr/lib/jvm/java-8-oracle' >> $v_bash_profile
 echo 'export PATH=$PATH:$JAVA_HOME/bin' >> $bash_profile
 echo 'export PATH=$PATH:$JAVA_HOME/bin' >> $v_bash_profile
-echo 'set -g -x JAVA_HOME /usr/lib/jvm/java-7-openjdk-i386' >> $fish_config
-echo 'set -g -x JAVA_HOME /usr/lib/jvm/java-7-openjdk-i386' >> $v_fish_config
+echo 'set -g -x JAVA_HOME /usr/lib/jvm/java-8-oracle' >> $fish_config
+echo 'set -g -x JAVA_HOME /usr/lib/jvm/java-8-oracle' >> $v_fish_config
 echo 'set -g -x PATH $PATH $JAVA_HOME/bin' >> $fish_config
 echo 'set -g -x PATH $PATH $JAVA_HOME/bin' >> $v_fish_config
 
